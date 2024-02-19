d28425fd1daa04cba2f0aed33e13154c146fa051
isaac-miles <isaacnice247@gmail.com>
2024-02-19 12:14:06 +0100

    react testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('render basic crude operation',()=>{
+  render(<App/>);
+  const titleText = screen.getByText(/Basic crud operation/i)
+  expect(titleText).toBeInTheDocument();
+})
