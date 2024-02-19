f11400230429def820f79f71eb8fa8b7ff7730e5
isaac-miles <isaacnice247@gmail.com>
2024-02-19 13:32:35 +0100

    .

--- src/component/__test__/Header.test.js
+++ src/component/__test__/Header.test.js
@@ -2,8 +2,18 @@ import { render, screen } from '@testing-library/react';
 import Heading from '../header';
 
 
-test("renders heading prop correctly",()=>{
+test("renders heading prop correctly",async ()=>{
     render(<Heading title='Testing the header'/>);
     const headingElement = screen.getByText(/Testing the header/i);
     expect(headingElement).toBeInTheDocument();
+})
+
+test("renders the heading  role",async ()=>{
+    render(<Heading title='Testing the header'/>);
+    const headingElement = screen.getByRole("heading",{name:"Test"});
+    expect(headingElement).toBeInTheDocument();
+})
+
+test("getting by title",async()=>{
+    render(<Heading />)
 })
--- src/component/header.tsx
+++ src/component/header.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Heading({title}:{title:string}) {
   return (
+    <>
     <h1>{title}</h1>
+    <h3>Test</h3>
+    </>
   )
 }
 
