344103f556ca926c8d228aa3d64a07171e1db4e7
isaac-miles <isaacnice247@gmail.com>
2024-02-19 20:50:51 +0100

    .

--- src/component/__test__/ListItems.test.js
+++ src/component/__test__/ListItems.test.js
@@ -12,6 +12,13 @@ const MockedItemList = ({items,completedItems})=>{
 test("should render the correct amount of Active task", async()=>{
     render(<MockedItemList items={3} completedItems={1} />);
 
-    const activeTasks = await screen.findByText('active-task')
+    const activeTasks = await screen.findByText(/active task/i)
+    expect(activeTasks).toBeInTheDocument();
+})
+
+test("should render the correct amount of completed task", async()=>{
+    render(<MockedItemList items={3} completedItems={1} />);
+
+    const activeTasks = await screen.findByText(/completed task/i)
     expect(activeTasks).toBeInTheDocument();
 })
