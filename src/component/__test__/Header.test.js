213bb60d5dd9276cddb69cffd9a0b04f76fd3ec4
isaac-miles <isaacnice247@gmail.com>
2024-02-19 13:41:56 +0100

    .

--- src/component/__test__/Header.test.js
+++ src/component/__test__/Header.test.js
@@ -2,33 +2,33 @@ import { render, screen } from '@testing-library/react';
 import Heading from '../header';
 
 
-test("renders heading prop correctly",async ()=>{
+test("renders heading prop correctly", ()=>{
     render(<Heading title='Testing the header'/>);
     const headingElement = screen.getByText(/Testing the header/i);
     expect(headingElement).toBeInTheDocument();
 })
 
-test("renders the heading  role",async ()=>{
+test("renders the heading  role", ()=>{
     render(<Heading title='Testing the header'/>);
     const headingElement = screen.getByRole("heading",{name:"Test"});
     expect(headingElement).toBeInTheDocument();
 })
 
-test("getting by title",async()=>{
+test("getting by title",()=>{
     render(<Heading />)
     const title = screen.getByTitle('header')
     expect(title).toBeInTheDocument();
 })
 
-test("getting by test-id",async()=>{
+test("getting by test-id",()=>{
     render(<Heading />)
     const testId = screen.getByTestId('header-2')
     expect(testId).toBeInTheDocument();
 })
 
-//Find by
+//Find by, this is async call
 test("renders same  prop passed to it correctly",async ()=>{
     render(<Heading title='Testing the header'/>);
-    const headingElement = screen.getByText(/Testing the header/i);
+    const headingElement = await screen.findByText(/Testing the header/i);
     expect(headingElement).toBeInTheDocument();
 })
