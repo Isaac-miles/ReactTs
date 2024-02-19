9083238208f3f7a8198f1e666f3a2e6db6920ac8
isaac-miles <isaacnice247@gmail.com>
2024-02-19 12:26:30 +0100

    wrote the test script for the header

--- src/component/__test__/Header.test.js
+++ src/component/__test__/Header.test.js
@@ -2,4 +2,8 @@ import { render, screen } from '@testing-library/react';
 import Heading from '../header';
 
 
-test("renders heading prop correctly")
+test("renders heading prop correctly",()=>{
+    render(<Heading title='Testing the header'/>);
+    const headingElement = screen.getByText(/Testing the header/i);
+    expect(headingElement).toBeInTheDocument();
+})
